cc4cca1e612d2bbcb2791e5c7aadbfc3611ecdf3
Lars Kue <lars@kuehmichel.de>
2018-07-20 11:56:17 +0200

    Clicking the Extension Icon now enables/disables the extension instead of opening YouTube in a new tab

--- background.js
+++ background.js
@@ -1,5 +1,23 @@
 // this is called when the user clicks on the browser extension icon
+let enabled = true;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({"url": "https://www.youtube.com/"});
+    // switches the enabled mode
+    enabled = !enabled;
+    if (enabled) {
+        chrome.browserAction.setIcon({path:"icon.png"})
+    }
+    else {
+        chrome.browserAction.setIcon({path:"iconDisabled.png"})
+    }
+
+    // opens YouTube in a new tab
+    // chrome.tabs.create({"url": "https://www.youtube.com/"});
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg === "getEnabled") {
+        sendResponse({enabled: enabled});
+        return true;
+    }
 });
--- content.js
+++ content.js
@@ -1,10 +1,22 @@
-const linkItem = document.createElement("a");
-linkItem.href="https://www.youtube.com/";
-linkItem.innerHTML = "<img src='https://imgur.com/gBgwnSa.png' title='YouTube Home'>";
-linkItem.setAttribute("style",
-    "position:fixed;" +
-    "top:15px;" +
-    "left:70px;" +
-    "z-index:20000;");
+// this is called everytime YouTube is loaded
 
-document.getElementsByTagName("body")[0].appendChild(linkItem);
+// check if the extension is enabled by sending a message to background.js
+chrome.runtime.sendMessage({msg: "getEnabled"}, function(response) {
+    if (response.enabled) {
+        execute();
+    }
+});
+
+
+function execute() {
+    const linkItem = document.createElement("a");
+    linkItem.href = "https://www.youtube.com/";
+    linkItem.innerHTML = "<img src='https://imgur.com/gBgwnSa.png' title='YouTube Home'>";
+    linkItem.setAttribute("style",
+        "position:fixed;" +
+        "top:15px;" +
+        "left:70px;" +
+        "z-index:20000;");
+
+    document.getElementsByTagName("body")[0].appendChild(linkItem);
+}
